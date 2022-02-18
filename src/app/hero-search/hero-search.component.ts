8bd79f53e5845bcb230df2b9ba00dde16b49a0c7
Sara Galstyan <sgalstyan@laberit.com>
2022-02-18 11:27:47 +0100

    my firtst commit

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { HeroService } from '../hero.service';
+import { IHero } from '../interfaces/i-hero';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.scss']
+})
+export class HeroSearchComponent implements OnInit {
+
+  heroes$!: Observable<IHero[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
+
+}
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {IHero} from  '../interfaces/i-hero';
+import { HeroService } from '../hero.service';
+//import { MessageService } from '../message.service'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+   /* hero: IHero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  */
+  heroes: IHero[] = [];
+  
+ // this.heroes = this.heroService.getHeroes();
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+
+  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+     .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as IHero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: IHero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+}
